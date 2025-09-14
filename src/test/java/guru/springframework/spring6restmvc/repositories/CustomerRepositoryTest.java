f2ff07ceab16bf35814ebbdf459b22bbfcfeaf9a
Srajan <csesrajanmishra@gmail.com>
2025-09-14 11:50:34 +0530

    Added Intial data

--- src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -1,9 +1,16 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.mappers.CustomerMapper;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
 import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,11 +22,36 @@ class CustomerRepositoryTest {
 
     @Test
     void testSaveCustomer() {
-        Customer customer = customerRepository.save(Customer.builder()
-                        .name("New Name")
-                .build());
+        CustomerMapper  customerMapper = Mappers.getMapper(CustomerMapper.class);
+        CustomerDTO customer1 = CustomerDTO.builder()
+                .name("Customer 1")
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
 
-        assertThat(customer.getId()).isNotNull();
+        CustomerDTO customer2 = CustomerDTO.builder()
+                .name("Customer 2")
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        CustomerDTO customer3 = CustomerDTO.builder()
+                .name("Customer 3")
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+        Customer c1 = customerMapper.customerDtoToCustomer(customer1);
+        Customer c2 = customerMapper.customerDtoToCustomer(customer2);
+        Customer c3 = customerMapper.customerDtoToCustomer(customer3);
+        Customer savedCustomer1 = customerRepository.save(c1);
+        Customer savedCustomer2 = customerRepository.save(c2);
+        Customer savedCustomer3 = customerRepository.save(c3);
+
+
+
+        assertThat(savedCustomer1.getId()).isNotNull();
+        assertThat(savedCustomer2.getId()).isNotNull();
+        assertThat(savedCustomer3.getId()).isNotNull();
 
     }
 }
